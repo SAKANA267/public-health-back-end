2a0ae5a31b327b3a669c108456b3fb6182e424ea
SAKANA'PC <ibert1ne@outlook.com>
2026-01-30 17:42:18 +0800

    feat(init):create springboot project

---
+++ src/test/java/com/publichealth/public_health_api/PublicHealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publichealth.public_health_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicHealthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
